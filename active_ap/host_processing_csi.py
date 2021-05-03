244701860506fa02c123c2b182b63ce54c733272
Ruichun <ruichun.ma@yale.edu>
2021-05-04 06:11:38 +0800

    real time ploting now

--- active_ap/host_processing_csi.py
+++ active_ap/host_processing_csi.py
@@ -7,6 +7,7 @@ UDP_IP = "192.168.4.2"
 UDP_PORT = 8848
 
 QUEUE_LEN = 50
+CSI_LEN = 57 * 2
 
 def parse_data_line (line, data_len) :
     data = line.split(",") # separate by commas
@@ -88,11 +89,30 @@ def cook_csi_data (rx_ctrl_info, raw_csi_data) :
 if __name__ == "__main__":
     # a queue to hold SNR values
     rssi_que = collections.deque(np.zeros(QUEUE_LEN))
+    csi_points = np.ones(CSI_LEN)
 
     # create plot figure
     fig = plt.figure(figsize=(12,6))
-    ax_rssi = plt.subplot(211)
-    ax_csi = plt.subplot(212)
+    ax_rssi = plt.subplot(121)
+    ax_csi = plt.subplot(122)
+
+    # animated=True tells matplotlib to only draw the artist when we
+    # explicitly request it
+    (curve_rssi,) = ax_rssi.plot(rssi_que, animated=True)
+    ax_rssi.set_ylim(-70, -20)
+    scatter_rssi = ax_rssi.scatter( len(rssi_que)-1, rssi_que[-1], animated=True)
+    text_rssi = ax_rssi.text( len(rssi_que)-1, rssi_que[-1]+2, "{} dB".format(rssi_que[-1]), animated=True)
+
+    (curve_csi,) = ax_csi.plot(csi_points, animated=True)
+    ax_csi.set_ylim(0, 50)
+
+    # make sure the window is raised, but the script keeps going
+    plt.show(block=False)
+    plt.pause(0.1) # stop to render backgroud
+
+    # get clean backgroud
+    bg = fig.canvas.copy_from_bbox(fig.bbox)
+
 
     # create a recv socket for packets from ESP32 soft-ap
     sock = socket.socket(socket.AF_INET, # Internet
@@ -114,22 +134,35 @@ if __name__ == "__main__":
         # prepare csi data
         (rssi, csi_data) = cook_csi_data(rx_ctrl_data, raw_csi_data)
 
-        # ax_rssi.cla()
-        # rssi_que.popleft()
-        # rssi_que.append( rssi )
-        # ax_rssi.plot(rssi_que)
-        # ax_rssi.scatter( len(rssi_que)-1, rssi_que[-1] )
-        # ax_rssi.text( len(rssi_que)-1, rssi_que[-1]+2, "{} dB".format(rssi_que[-1]) )
-        # ax_rssi.set_ylim(-80, -40)
+        # update RSSI
+        rssi_que.popleft()
+        rssi_que.append( rssi )
+        # update CSI
+        csi_points = 10 * np.log10(np.abs(csi_data)**2)
 
-        # ax_csi.cla()
-        # csi_points = 10 * np.log10(np.abs(csi_data)**2)
-        # ax_csi.plot(csi_points)
-        # ax_csi.set_ylim(0, 50)
+        # plot rssi and CSI
+        # 1. restore background
+        fig.canvas.restore_region(bg)
+        # 2. update artist class instances
+        curve_rssi.set_ydata(rssi_que)
+        scatter_rssi.set_offsets( (len(rssi_que)-1 , rssi_que[-1]) )
+        text_rssi.set_position( (len(rssi_que)-1, rssi_que[-1]+2) )
+        text_rssi.set_text("{} dB".format(rssi_que[-1]) )
+        curve_csi.set_ydata(csi_points)
+        # 3. draw
+        ax_rssi.draw_artist(curve_rssi)
+        ax_rssi.draw_artist(scatter_rssi)
+        ax_rssi.draw_artist(text_rssi)
+        ax_csi.draw_artist(curve_csi)
 
-        plt.pause(0.001)
 
-    plt.show()
+        # copy the image to the GUI state, but screen might not be changed yet
+        fig.canvas.blit(fig.bbox)
+        # flush any pending GUI events, re-painting the screen if needed
+        fig.canvas.flush_events()
+        
+        # you can put a pause in if you want to slow things down
+        plt.pause(0.01)
     
 
 
